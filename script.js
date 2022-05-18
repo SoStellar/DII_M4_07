16b3ca40a09341b9823dd8f8b4c54945a99f23f9
SoStellar <githubpkaddress@gmail.com>
2022-05-18 12:22:37 +0700

    cancel alert

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const newText = 'ok';
-document.getElementById('cancel').innerHTML = newText;
+function alert1(){
+    alert('let me see...');
+}
+let ok = document.getElementById('ok');
+ok.addEventListener('click', alert1);
+function alert2(){
+    alert('not ok...');
+}
+let cancel = document.getElementById('cancel');
+cancel.addEventListener('click', alert2);
